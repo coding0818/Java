e0e7c70884548ff77fc8ddb3277333c97744292f
coding0818 <coding0818@gmail.com>
2022-08-21 22:13:20 +0900

    2022/08/21 step2까지

--- Backjoon/src/step02/_05.java
+++ Backjoon/src/step02/_05.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 /*
  * 날짜 : 2022/08/21
  * 이름 : 박가영
- * 내용 : 2단계 4번 문제
+ * 내용 : 2단계 5번 문제
  */
 public class _05 {
 	public static void main(String[] args) {
@@ -15,27 +15,25 @@ public class _05 {
 		int H = sc.nextInt();
 		int M = sc.nextInt();
 		
-		if (H <= 23 && H >= 0 && M <= 59 && M >= 0);
-		
-		if(H > 0 && M < 45 ) { 
-			System.out.println((H - 1)+" "+(60-(45 - M)));
-		}
-		else if(H > 0 && M > 45 ) {
-			System.out.println(H+" "+(M - 45));
-		}
-		else if(H > 0 && M == 45) {
-			System.out.println(H);
-		}
-		else if(H == 0 && M < 45) {
-			System.out.println("23 "+(60-(45 - M)));
-		}
-		else if(H == 0 && M > 45) {
-			System.out.println(" "+(M - 45));
-		}
-		else if(H == 0 && M == 45) {
-			System.out.println("");
-		}
-		
+		if (H <= 23 && H >= 0 && M <= 59 && M >= 0) {
+			if(H > 0) {
+			if(M < 45) { 
+				H = H - 1;
+				M = M + 15;}
+			else {
+				M = M - 45;}
+			}
+			
+			else if(H == 0) {
+			if(M < 45) {
+				H = 23;
+				M = M + 15;}
+			else if(M >= 45) {
+				M = M - 45;}
+			}
+			
+			System.out.printf("%d %d", H, M);}
+	   }
 	}
 
-}
+
--- Backjoon/src/step02/_06.java
+++ Backjoon/src/step02/_06.java
@@ -1,5 +1,35 @@
 package step02;
 
+import java.util.Scanner;
+
+/*
+ * 날짜 : 2022/08/21
+ * 이름 : 박가영
+ * 내용 : 2단계 6번 문제
+ */
 public class _06 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		int C = sc.nextInt();
+		
+		if(A <= 23 && A >= 0 && B >= 0 && B <= 59 && C >= 0 && C <= 1000);
+		
+		if (C % 60 == 0) {
+			if((A + ( C / 60 ))>=24) {
+			A = A + ( C / 60 ) - 24;
+			}
+		}
+		
+		if (C % 60 != 0) {
+			
+			A = A + ( C / 60 );
+			B = B + ( B % 60 );
+		}
+		
+		System.out.printf("%d %d", A, B);
+	}
 
 }
