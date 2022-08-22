1976f7cf302dbeef634ef91209c8c34aa3bb3ddb
parkgayoung96 <lunatic2563@gmail.com>
2022-08-22 19:52:01 +0900

    2022/08/22 2번까지

---
+++ Backjoon/src/step03/_02.java
@@ -0,0 +1,29 @@
+package step03;
+
+import java.util.Scanner;
+
+/*
+ * 날짜 : 2022/08/22
+ * 이름 : 박가영
+ * 내용 : 3단계 2번 문제
+ */
+public class _02 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int T = sc.nextInt();
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		
+		if(A > 0 && B > 0);
+		
+		for(int i=1; i<=5; i++) {
+			
+			int C = A + B;
+			System.out.println(C);
+		}
+		
+	}
+
+}
