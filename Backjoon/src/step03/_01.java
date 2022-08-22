dbaf3aeccf6e8f343da6d825cd8f51b3ada9a66e
parkgayoung96 <lunatic2563@gmail.com>
2022-08-22 19:40:27 +0900

    step3 1번까지

--- Backjoon/src/step03/_01.java
+++ Backjoon/src/step03/_01.java
@@ -1,9 +1,27 @@
 package step03;
+
+import java.util.Scanner;
+
 /*
  * 날짜 : 2022/08/22
  * 이름 : 박가영
  * 내용 : 3단계 1번 문제
  */
 public class _01 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		if(N >= 1 && N <= 9);
+		
+		
+			for(int y = 1; y <= 9; y++) {
+				int z = N * y;
+				System.out.println(N +" * "+ y +" = "+ z);
+			}
+		}
+	}
 
-}
+
