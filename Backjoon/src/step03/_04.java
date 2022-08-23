19064d88c6d543bfb9ce86d1b7c1707eb27bbbb9
parkgayoung96 <lunatic2563@gmail.com>
2022-08-23 16:53:33 +0900

    2022/08/23 4번까지

--- Backjoon/src/step03/_04.java
+++ Backjoon/src/step03/_04.java
@@ -1,4 +1,7 @@
 package step03;
+
+import java.util.Scanner;
+
 /*
  * 날짜 : 2022/08/22
  * 이름 : 박가영
@@ -6,7 +9,33 @@ package step03;
  */
 public class _04 {
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int X = sc.nextInt();
+		int N = sc.nextInt();
+		if(X >= 1 && X <= 1000000000 && N >= 1 && N <= 100);
+		
+				
+		for(int i=1; i <= N; i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			if(a >= 1 && a <= 1000000 && b >= 1 && b <= 10);
+			
+			X = X - (a*b);
+			}
+           
+		
+			
+		if(X == 0) {
+			System.out.println("Yes");
+		}
+		else {
+		    System.out.println("No");
+		}
+			
 		
 	}
-
+		
 }
+
+
