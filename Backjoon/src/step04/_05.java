757a239bc7aac4435086792dfd9dac5c67f36574
coding0818 <coding0818@gmail.com>
2022-08-28 14:52:33 +0900

    2022/08/28

--- Backjoon/src/step04/_05.java
+++ Backjoon/src/step04/_05.java
@@ -1,12 +1,34 @@
 package step04;
+
+import java.util.Scanner;
+
 /*
-* 날짜 : 2022/08/26
+* 날짜 : 2022/08/28
 * 이름 : 박가영
 * 내용 : 4단계 5번 점수를 구하는 프로그램 작성
  */
 public class _05 {
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		
+		int n = sc.nextInt();
+		String arr[] = new String [n];
+		int score = 0;
+		int sum = 0;
+		for(int i=0; i<n; i++) {
+			arr[i] = sc.nextLine();
+			String[] OX = arr[i].split("");
+			for(int k=0; k<OX.length; k++) {
+				if(OX[k]=="O") {
+					score++;
+					sum += score;
+				}
+				else {
+					score = 0;
+				}
+			}
+		}
+		System.out.println(sum);
 	}
 
 }
---
+++ Backjoon/src/step04/_06.java
@@ -0,0 +1,13 @@
+package step04;
+/*
+* 날짜 : 2022/08/28
+* 이름 : 박가영
+* 내용 : 4단계 6번 평균을 넘는 학생 비율 구하기
+ */
+public class _06 {
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
