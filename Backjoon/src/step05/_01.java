3a5b90d7eb86d62eafb0779999616d540aaa7529
parkgayoung96 <lunatic2563@gmail.com>
2022-08-30 17:31:38 +0900

    2022/08/30 step5 num2

--- Backjoon/src/step05/_01.java
+++ Backjoon/src/step05/_01.java
@@ -5,5 +5,16 @@ package step05;
  * 내용 : 5단계 1번 정수 n개의 합
  */
 public class _01 {
-
+	public static long sum(int[] a) {
+		
+	    long sum = 0;
+		
+		for(int i=0;i<a.length;i++) {
+			sum += a[i];
+		}
+		
+		return sum;
+	}
 }
+
+
