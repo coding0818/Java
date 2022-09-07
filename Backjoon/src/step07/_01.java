fb0c4719a85437b8e3167c53c609dc103758d94f
parkgayoung96 <lunatic2563@gmail.com>
2022-09-07 19:17:48 +0900

    2022/09/07

--- Backjoon/src/step07/_01.java
+++ Backjoon/src/step07/_01.java
@@ -3,7 +3,7 @@ package step07;
 import java.util.Scanner;
 
 /*
- * 날짜 : 2022/09/05
+ * 날짜 : 2022/09/07
  * 이름 : 박가영
  * 내용 : 7단계 1번 손익분기점을 구하는 프로그램 만들기
  */
@@ -15,29 +15,24 @@ public class _01 {
 		int b = sc.nextInt();
 		int c = sc.nextInt();
 		
-		breakpoint(a,b,c);
 		
-	}
-	
-	
-	public static void breakpoint(int a, int b, int c) {
-		int n1 = a/(c-b);
-		int i[] = new int[2100000001];
-		
-		for(int k=0;k<i.length;k++) {
-			i[k]=k+1;
-		}
 		if(b>=c) {
 			System.out.println("-1");
 		}		
-		else{for(int j=0; j<i.length;j++) {
-		   if(n1<i[j]) {
-			   System.out.println(i[j]);
-			   break;
-		   }
-		}   
-	   }
+		else if(c-b==1){
+			System.out.println(a+1);}
+		else {
+			 int n1 = a/(c-b);
+			 for(int j=1; j<2100000001;j++) {
 		
-
-    }
+		        if(n1<j) {
+			       System.out.println(j);
+			       break;
+		        }
+		     }   
+	    }
+	
+	
+	}
+    
 }
---
+++ Backjoon/src/step07/_02.java
@@ -0,0 +1,35 @@
+package step07;
+
+import java.util.Scanner;
+
+/*
+ * 날짜 : 2022/09/07
+ * 이름 : 박가영
+ * 내용 : 7단계 2번 벌집이 형성되는 규칙에 따라 벌집의 위치를 구하는 문제
+ */
+public class _02 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();	
+		
+		bee(n);
+	}
+	
+	public static void bee(int n) {
+		
+		int max = 0;
+		for(int i=1;i<=3;i++) {
+				max += i;
+		}
+		int a = 6*max+1;
+		int a2 = 
+		while(n<a) {
+			
+		}
+		
+	} 	
+
+}
+
+
