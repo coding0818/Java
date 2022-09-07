0aa28c3c423842c1f9dd2d5df1ddbcc0ac3bf703
parkgayoung96 <lunatic2563@gmail.com>
2022-09-07 19:52:54 +0900

    2022/09/07 step7

--- Backjoon/src/step07/_02.java
+++ Backjoon/src/step07/_02.java
@@ -12,24 +12,26 @@ public class _02 {
 		Scanner sc = new Scanner(System.in);
 		
 		int n = sc.nextInt();	
+		int max = 0;
+		int min = 0;
+		for(int j=1; j>0; j++) {
+			for(int i=1;i<=j;i++) {
+				max += i;
+		    }
+		    for(int k=1; k<=j-1; k++) {
+		         min += k;
+  		    }
+		    int a = 6*max+1;
+		    int b = 6*min+1;
+		    if(n<a && n>b) {
+			     System.out.println(max-min);
+			     break;
+		    }
+		    
+		}
 		
-		bee(n);
 	}
 	
-	public static void bee(int n) {
-		
-		int max = 0;
-		for(int i=1;i<=3;i++) {
-				max += i;
-		}
-		int a = 6*max+1;
-		int a2 = 
-		while(n<a) {
-			
-		}
-		
-	} 	
-
 }
 
 
