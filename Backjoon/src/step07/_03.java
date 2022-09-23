f352e6dacf51815ea1baf5419356b5aefc3ab553
parkgayoung96 <lunatic2563@gmail.com>
2022-09-23 19:27:41 +0900

    2022/09/23

--- Backjoon/src/step07/_03.java
+++ Backjoon/src/step07/_03.java
@@ -3,7 +3,6 @@ package step07;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.lang.reflect.Array;
 
 /*
  * 날짜 : 2022/09/08
@@ -14,17 +13,31 @@ public class _03 {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int x = Integer.parseInt(br.readLine());
-		String a[] = new String [x];
+		int n=0;
+		int count = 0;
+		int a[] = new int [x];
+		int b[] = new int [x];
 		
 		
-		for(int n=0; n<a.length; n++) {
-			a[0] = 1+"/"+1;
-			a[1] = 1+"/"+2;
-			a[2] = 2+"/"+1;
+		for(int j=0; j<=a.length; j++) {
+			count += j;
+			if(count > x) {
+				n = count-x;
+				if(n != -1) {
+					a[n] = j-n;
+					b[n] = n+1;
+					System.out.println(a[n]+"/"+b[n]);
+				}else {
+					System.out.println("1/1");
+				}
+				break;
+			}else if(count == x) {
+				a[n] = j;
+				System.out.println(a[n]+"/1");
+				break;
+			}
 		}
 		
+		br.close();
 	}
-	
-	
-
 }
