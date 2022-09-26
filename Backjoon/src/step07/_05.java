3726ac907c6e2ed094c85e4f71fa2d32f3dfd4ff
parkgayoung96 <lunatic2563@gmail.com>
2022-09-26 14:58:16 +0900

    2022/09/26

--- Backjoon/src/step07/_05.java
+++ Backjoon/src/step07/_05.java
@@ -1,8 +1,7 @@
 package step07;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 /*
  * 날짜 : 2022/09/23
@@ -12,14 +11,20 @@ import java.io.InputStreamReader;
 public class _05 {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		Scanner sc = new Scanner(System.in);
 		
-		int t = Integer.parseInt(br.readLine());
-		
-		
+		int t = sc.nextInt();
 		
 		for(int i=1; i<=t; i++) {
-			int h = Integer.parseInt(br.)
+			int h = sc.nextInt();
+			int w = sc.nextInt();
+			int n = sc.nextInt();
+			int a = n % h;
+			double b1 = (double)n/h;
+			double b = Math.ceil(b1);
+			
+			int room = (int) (a*100+b);
+			System.out.println(room);
 		}
 		
 	}
