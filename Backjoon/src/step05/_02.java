757739957d6fcb33754c0390c6f2d0b5f8f7042a
parkgayoung96 <lunatic2563@gmail.com>
2022-09-30 19:40:47 +0900

    2022/09/30

--- Backjoon/src/step05/_02.java
+++ Backjoon/src/step05/_02.java
@@ -1,4 +1,8 @@
 package step05;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /*
  * 날짜 : 2022/08/30
  * 이름 : 박가영
@@ -6,31 +10,30 @@ package step05;
  */
 public class _02 {
 	public static void main(String[] args) {
-		int[]y= new int[10000];
+		ArrayList<Integer> arr = new ArrayList<Integer>();
+		ArrayList<Integer> self = new ArrayList<Integer>();
 		
-		for(int i=0;i<y.length;i++) {
-			y[i]=i+1;
-	    }
+		for(int i=1; i<=10000; i++) {
+			arr.add(i);
+		}
 		
-			
-		for(int j=0;j<10000;j++) {
-			
-			if(y[j]!=self(j)) { 
-			System.out.println(y[j]);
+		for(int num=1; num<=10000; num++) {
+			self.add(num + (num/1000) + (num/100)%10 + (num/10)%10 + num % 10);
+		}
+		
+		List<Integer> result = new ArrayList<Integer>();
+		for(int i=1; i<self.size(); i++) {
+			for(int j=1; j<arr.size(); j++) {
+				if(!self.get(i).equals(arr.get(j))) {
+					result.add(arr.get(i));
+					System.out.println(result.get(i));
+				}	
 			}
+			
 		}
 		
-	}
-	
-	public static int self(int num) {
-		int sum = num;
 		
-		while(num>0) {
-			sum += num % 10;
-			num/=10;
-		}
-		return sum;
+		
 	}
-
 	
 }
